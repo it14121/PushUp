b7512358278e43cd61c3e826e60d200da3f9fa53
it14121 <it14121@uom.edu.gr>
2016-12-29 13:44:31 +0200

    Modified MainActivity Added new File "TestAddFile.java"

--- app/src/main/java/com/example/pushup/MainActivity.java
+++ app/src/main/java/com/example/pushup/MainActivity.java
@@ -52,7 +52,8 @@ public class MainActivity extends AppCompatActivity {
 
     private void testUpload(){
 
-        int i = 0;
-        i = i + 1;
+        int i = 1 + 1;
+        int ii = 2+2;
+        int iii = i + ii;
     }
 }
---
+++ app/src/main/java/com/example/pushup/TestAddFile.java
@@ -0,0 +1,8 @@
+package com.example.pushup;
+
+/**
+ * Created by Ειρήνη Καραγεώργη on 29/12/2016.
+ */
+
+public class TestAddFile {
+}
